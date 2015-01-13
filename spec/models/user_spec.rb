7a485854ab10d9ed1d41ca6dfd593e4814fd2d3f
jens1990 <jenspasslick@googlemail.com>
2015-01-13 17:16:36 +0100

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,20 +15,20 @@ describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
-                            password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
-
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
   it { should respond_to(:admin) }
-  it { should respond_to(:authenticate) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -42,29 +42,42 @@ describe User do
     it { should be_admin }
   end
 
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
+  describe "micropost associations" do
 
-  describe "remember token" do
     before { @user.save }
-    its(:remember_token) { should_not be_blank }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
   end
 
-  describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
+  it "should destroy associated microposts" do
+    microposts = @user.microposts.dup
+    @user.destroy
+    microposts.should_not be_empty
+    microposts.each do |micropost|
+      Micropost.find_by_id(micropost.id).should be_nil
+    end
   end
 
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
-
-  describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil }
-    it { should_not be_valid }
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
   end
 
   describe "return value of authenticate method" do
@@ -83,18 +96,18 @@ describe User do
     end
   end
 
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-
-  describe "when email is not present" do
-    before { @user.email = " " }
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 
@@ -119,6 +132,11 @@ describe User do
     end
   end
 
+  describe "when name is not present" do
+    before { @user.name = "a" * 51}
+    it { should_not be_valid }
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -129,4 +147,14 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
 end
